1f544aecb5a9d813728d2cae39e671b0f58523ed
Emil Meiton <emilmeiton@gmail.com>
2016-07-05 13:45:10 +0200

    commiyt

---
+++ Assets/Scripts/ForwardMovement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ForwardMovement : MonoBehaviour {
+    Rigidbody rb;
+    public float force;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        rb.AddForce(transform.forward*force*Mathf.PerlinNoise(Time.time,0.0f));
+	}
+}
