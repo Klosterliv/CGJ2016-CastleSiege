a1090708552a326db4bc0b3447c65c9febdf12e2
Klosterliv <nekobebis@gmail.com>
2016-07-07 20:56:08 +0200

    projectile enhancements

---
+++ Assets/Scripts/ProjectileTrailEffect.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileTrailEffect : MonoBehaviour {
+
+	public Transform projectile;
+	Rigidbody projectileRigid;
+	Vector3 projectileV;
+	bool dying = false;
+	float deathtimer = 6f;
+
+	void Start() {
+		projectileRigid = (Rigidbody)projectile.GetComponent(typeof(Rigidbody));
+		projectileV = new Vector3(0,0,0);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (dying) {
+			if (deathtimer <= 0) {
+				Destroy(gameObject);
+			}
+			else deathtimer-=Time.deltaTime;
+		}
+		else Follow();
+	}
+
+	void Follow() {
+		if (projectile != null) {
+			transform.position = Vector3.Lerp(transform.position, projectile.position, Time.deltaTime*50*Vector3.Distance(transform.position, projectile.position));
+			projectileV = projectileRigid.velocity;
+		}			
+		else {
+			Rigidbody rigid = (Rigidbody) GetComponent(typeof(Rigidbody));
+			rigid.useGravity = true;
+			rigid.velocity = projectileV;
+			dying = true;
+		}
+	}
+}
