8a5240b6e930d79b6bc5681ddaaf5fd254e85a1c
Emil Meiton <emilmeiton@gmail.com>
2016-07-08 01:53:04 +0200

    hbkhol

--- Assets/Scripts/Attraction.cs
+++ Assets/Scripts/Attraction.cs
@@ -33,7 +33,14 @@ public class Attraction : MonoBehaviour {
         {
             loopCounter = 0;
             pac = GetComponent<PanicAgentController>();
+
+            if (rb.velocity.magnitude < 0.08f)
+            {
+                pac.panicStrength += 0.3f;
+            }
+
             lookRay = new Ray(transform.position, new Vector3(Random.insideUnitCircle.x, 0, Random.insideUnitCircle.y));
+            Debug.DrawRay(lookRay.origin, lookRay.direction*findNeighbourDistance);
             if (Physics.Raycast(lookRay, out hitInfo, findNeighbourDistance, agentsLayer)) // finding something
             {
                 if(hitInfo.transform.GetComponent<State>().currentState == State.aiState.attacking)
@@ -64,7 +71,7 @@ public class Attraction : MonoBehaviour {
             {
                 newQuat = Quaternion.LookRotation(transform.position - hitInfo.transform.position);
                 transform.rotation = Quaternion.Lerp(transform.rotation, newQuat, 0.3f);
-                pac.panicStrength += 1.0f;
+                pac.panicStrength += 1.6f;
             }
         }
 	}
