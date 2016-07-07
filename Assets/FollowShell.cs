24c31f2462ff15b139cd2bf8f09fdbd645b9d746
Stoil Stoichev <stoilstoychev@gmail.com>
2016-07-07 15:16:50 +0200

    camera

---
+++ Assets/FollowShell.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowShell : MonoBehaviour
+{
+    Transform shellCam;
+
+    // Use this for initialization
+    void Start()
+    {
+        //   HideShellMonitor();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (shellCam == null)
+            HideShellMonitor();
+    }
+
+    public void ShowShellMonitor()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void HideShellMonitor()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void RegisterShellCam(Transform shellCam)
+    {
+        this.shellCam = shellCam;
+    }
+
+    public bool isShellVIew()
+    {
+        if (shellCam == null)
+            return false;
+
+          print(shellCam.GetComponent<Camera>().targetTexture);
+
+        if (shellCam.GetComponent<Camera>().targetTexture == null)
+            return true;
+
+        return false;
+    }
+
+    private void SwitchToShellView()
+    {
+
+    }
+
+    void OnMouseDown()
+    {
+        shellCam.GetComponent<Camera>().targetTexture = null;
+    }
+}
--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -1,53 +1,64 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CameraControl : MonoBehaviour {
+public class CameraControl : MonoBehaviour
+{
 
-	[SerializeField]
-	float zoomSpeed = 1;
-	[SerializeField]
-	float camSpeed = 1;
-	[SerializeField]
-	float maxHeight = 2;
-	[SerializeField]
-	float minHeight = 1;
-	[SerializeField]
-	float camLerpSpeed = 1;
-	[SerializeField]
-	LayerMask terrainMask;
+    [SerializeField]
+    float zoomSpeed = 1;
+    [SerializeField]
+    float camSpeed = 1;
+    [SerializeField]
+    float maxHeight = 2;
+    [SerializeField]
+    float minHeight = 1;
+    [SerializeField]
+    float camLerpSpeed = 1;
+    [SerializeField]
+    LayerMask terrainMask;
 
-	Vector3 targetPos;
+    Vector3 targetPos;
 
-	// Use this for initialization
-	void Start () {
-		targetPos = transform.position;
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    void Awake()
+    {
+        transform.FindChild("ShellMonitor").gameObject.SetActive(true);
+    }
 
-		MoveCamera();
+    // Use this for initialization
+    void Start()
+    {
 
-	}
+        targetPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        MoveCamera();
+
+    }
 
 
-	void MoveCamera() {
+    void MoveCamera()
+    {
 
-		Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-		move *= camSpeed;
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        move *= camSpeed;
 
-		targetPos = transform.position+move;
+        targetPos = transform.position + move;
 
-		//transform.Translate(move);
-		RaycastHit hit;
-		Ray ray = new Ray(transform.position, -Vector3.up);
-		//Debug.DrawRay(ray);
-		if (Physics.Raycast(ray, out hit, 2000, terrainMask)) {
-			//Debug.Log(hit.distance);
-			//if (hit.distance <= minHeight)
-			targetPos.y = hit.point.y + minHeight;
-		}
+        //transform.Translate(move);
+        RaycastHit hit;
+        Ray ray = new Ray(transform.position, -Vector3.up);
+        //Debug.DrawRay(ray);
+        if (Physics.Raycast(ray, out hit, 2000, terrainMask))
+        {
+            //Debug.Log(hit.distance);
+            //if (hit.distance <= minHeight)
+            targetPos.y = hit.point.y + minHeight;
+        }
 
-		transform.position = Vector3.Lerp(transform.position, targetPos, Time.deltaTime*camLerpSpeed);
-	}
+        transform.position = Vector3.Lerp(transform.position, targetPos, Time.deltaTime * camLerpSpeed);
+    }
 }
